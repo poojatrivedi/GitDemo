ea6fea49cab36eb4c0a678838dc667aaa2a0e8bc
pooja trivedi <poojatrivedi@aurumcommunications.com>
2015-07-17 15:18:22 +0530

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Pooja Trivedi on 7/17/15.
+//  Copyright (c) 2015 aurum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
